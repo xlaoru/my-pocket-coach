874e6909ab9e58d69699a62ca087dcdca7c6f87b
xlaoru <sia212007@gmail.com>
2026-08-22 16:28:06 +0300

    Update disabling loading logic for link exercise to superset

--- app/(root)/(modals)/programs/[_id].tsx
+++ app/(root)/(modals)/programs/[_id].tsx
@@ -575,7 +575,7 @@ export default function Program() {
                                     onRetry={() => refetchProgram()}
                                 />
                             )
-                            : isProgramLoading || createExerciseMutation.isPending || createSupersetMutation.isPending || linkExerciseMutation.isPending
+                            : isProgramLoading || createExerciseMutation.isPending || createSupersetMutation.isPending
                                 ? (
                                     <Loader text="Loading your program..." />
                                 )

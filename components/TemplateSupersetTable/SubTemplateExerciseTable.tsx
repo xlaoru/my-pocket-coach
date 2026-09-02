83f746f5bb0b1f1ae5548ce6dbee5a6e3b53ceec
xlaoru <sia212007@gmail.com>
2026-09-02 16:08:24 +0300

    Fix no-gap issue

--- components/TemplateSupersetTable/SubTemplateExerciseTable.tsx
+++ components/TemplateSupersetTable/SubTemplateExerciseTable.tsx
@@ -129,6 +129,7 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         alignItems: "center",
         justifyContent: "space-between",
+        gap: 8,
     },
     headingContainer: {
         flex: 1,
--- components/TemplateSupersetTable/TemplateSupersetTable.tsx
+++ components/TemplateSupersetTable/TemplateSupersetTable.tsx
@@ -205,6 +205,7 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         alignItems: "center",
         justifyContent: "space-between",
+        gap: 8,
     },
     headingContainer: {
         flex: 1,
